065b92413df1a0f56539d840bfca6d249e042768
tormelo <23294699+tormelo@users.noreply.github.com>
2023-04-25 17:04:10 -0300

    feat: add is_palindrome_iterative function to challenge_palindromes_iterative.py module

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,14 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+
+    low_index = 0
+    high_index = len(word) - 1
+
+    while high_index - low_index > 0:
+        if word[low_index] != word[high_index]:
+            return False
+        low_index += 1
+        high_index -= 1
+
+    return True
