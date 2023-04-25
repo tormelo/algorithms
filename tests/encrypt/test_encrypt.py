96ada2681a0721249310ea182c1d8fa5dbc5bbf5
tormelo <23294699+tormelo@users.noreply.github.com>
2023-04-25 15:31:18 -0300

    test: add tests to the encrypt_message function

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    message = "morango"
+
+    with pytest.raises(TypeError):
+        encrypt_message(2, message)
+    assert encrypt_message(message, 10) == "ognarom"
+    assert encrypt_message(message, 3) == "rom_ogna"
+    assert encrypt_message(message, 4) == "ogn_arom"
