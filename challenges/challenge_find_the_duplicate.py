8419982d9a59fd26bf6b9c529c51ca20db184e36
tormelo <23294699+tormelo@users.noreply.github.com>
2023-04-25 16:44:52 -0300

    feat: add find_duplicate function to challenge_find_the_duplicate.py module

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,14 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or len(nums) == 1:
+        return False
+
+    nums.sort()
+
+    for i in range(len(nums) - 1):
+        if type(nums[i]) != int or nums[i] < 1:
+            return False
+
+        if nums[i] == nums[i + 1]:
+            return nums[i]
+
+    return False
