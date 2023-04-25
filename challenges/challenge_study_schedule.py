c70f56a8251964dcf6c84a44c79a93c8ec195719
tormelo <23294699+tormelo@users.noreply.github.com>
2023-04-25 15:11:01 -0300

    feat: add study_schedule function to challenge_study_schedule.py module

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not target_time:
+        return None
+
+    matches = 0
+    for entry, exit in permanence_period:
+        if type(entry) != int or type(exit) != int:
+            return None
+        if entry <= target_time <= exit:
+            matches += 1
+
+    return matches
